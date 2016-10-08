b33f16f7466df1900dd4ba8f25146219bf1081af
Dima Petruk <bavaria95@gmail.com>
2016-10-08 10:49:14 +0300

    scaling the last feature

--- knn/knn.py
+++ knn/knn.py
@@ -14,14 +14,23 @@ def knn(train, test1, labels, k=1):
     indexes_min = np.argsort([dist(x, test1) for x in train])[ :k]
     return vote(labels, indexes_min)
 
+def scale_feature(x, feature_number, k):
+    y = x.copy()
+    y[:, feature_number] *= k
+    return y
+
 if __name__ == "__main__":
     mat = read_yale()
-    correctly_classified = 0
-    for i in range(mat['featuresTest'].shape[0]):
-        predicted = knn(mat['featuresTrain'], mat['featuresTest'][i], mat['personTrain'], 1)
-        actual = mat['personTest'][i]
-        if actual == predicted:
-            correctly_classified += 1
 
-    print(float(correctly_classified) / mat['personTest'].shape[0])
+    for alpha in range(1, 21):
+        featuresTrain = scale_feature(mat['featuresTrain'], 9, alpha)
+        featuresTest = scale_feature(mat['featuresTest'], 9, alpha)
 
+        correctly_classified = 0
+        for i in range(featuresTest.shape[0]):
+            predicted = knn(featuresTrain, featuresTest[i], mat['personTrain'], 1)
+            actual = mat['personTest'][i]
+            if actual == predicted:
+                correctly_classified += 1
+
+        print("alpha = %s. acc = %s" % (alpha, float(correctly_classified) / mat['personTest'].shape[0]))
