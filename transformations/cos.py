11b911518ab2793da0e7a7e2b7c8ca9386af19da
Dima Petruk <bavaria95@gmail.com>
2016-11-04 18:46:44 +0000

    generating cosine signal by segments

--- transformations/cos.py
+++ transformations/cos.py
@@ -1,11 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def gen_signal():
-    x = np.linspace(-np.pi, np.pi, 20001)
+def gen_add_signal():
+    n_samples = 20000
+    x = np.linspace(-np.pi, np.pi, n_samples)
     f = np.zeros(x.shape)
     for w in [5, 10, 25, 50, 100]:
         f += np.cos(x*w)
+
+    return f
+
+def gen_segment_signal():
+    n_samples = 20000
+    x = np.linspace(-np.pi, np.pi, n_samples)
+    w = [5, 10, 25, 50, 100]
+    f = np.zeros(x.shape)
+    for i in range(5):
+        index_1 = i * (n_samples / 5)
+        index_2 = (i + 1) * (n_samples / 5)
+        f[index_1:index_2] += np.cos(x[index_1:index_2]*w[i])
 
     return f
 
@@ -14,4 +27,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-    gen_signal()
+    # gen_add_signal()
+    gen_segment_signal()
