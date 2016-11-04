7ffcf596a9cc53e077fee6d613483fb058bb6456
Dima Petruk <bavaria95@gmail.com>
2016-11-04 21:40:11 +0000

    rewritten signal generator

--- transformations/cos.py
+++ transformations/cos.py
@@ -1,14 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def gen_add_signal():
-    n_samples = 20000
-    x = np.linspace(-np.pi, np.pi, n_samples)
-    f = np.zeros(x.shape)
-    for w in [5, 10, 25, 50, 100]:
-        f += np.cos(x*w)
+def generate_cosine(ff):
+    Fs = 2000                        # sampling rate
+    Ts = 1.0/Fs                      # sampling interval
+    t = np.arange(0, 1, Ts)          # time vector
+    y = np.cos(2 * np.pi * ff * t)
 
-    return f
+    return (t, y)
+
+def gen_add_signal():
+    f = np.zeros(2000)
+    for w in [5, 10, 25, 50, 100]:
+        t, y = generate_cosine(w)
+        f += y
+
+    return (t, f)
 
 def gen_segment_signal():
     n_samples = 20000
@@ -20,6 +27,8 @@ def gen_segment_signal():
         index_2 = (i + 1) * (n_samples / 5)
         f[index_1:index_2] += np.cos(x[index_1:index_2]*w[i])
 
+    plt.plot(x, np.fft.fft(f))
+    plt.show()
     return f
 
 def main():
@@ -27,5 +36,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # gen_add_signal()
-    gen_segment_signal()
+    gen_add_signal()
+    # gen_segment_signal()
