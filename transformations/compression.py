4f84b46a8c6387a6b3355c0b8fcc2ff6f01e3d37
Dima Petruk <bavaria95@gmail.com>
2016-11-08 22:35:44 +0100

    decompressing image

--- transformations/compression.py
+++ transformations/compression.py
@@ -1,14 +1,13 @@
 import numpy as np
 import pywt
 import cv2
+import copy
 
-eps = 5
-
-def compress_image(A, levels=3):
+def compress_image(A):
     for i in range(A.shape[0]):
         x = A[i].astype(float)
         n = x.shape[0]
-        for _ in range(levels):
+        for _ in range(int(np.log2(n))):
             cA, cD = pywt.dwt(x[ :n], 'haar')
             x[0:n/2] = cA
             x[n/2:n] = cD
@@ -16,8 +15,22 @@ def compress_image(A, levels=3):
 
         A[i] = x
 
+def decompress_image(A):
+    for i in range(A.shape[0]):
+        x = A[i].astype(float)
+        n = 1
+        for _ in range(int(np.log2(x.shape))):
+            cA = x[0:n]
+            cD = x[n:2*n]
+            x[ :2*n] = pywt.idwt(cA, cD, 'haar')
+            n *= 2
 
-A = cv2.imread('images/image3.png')[:, :, 0].astype(float)
+        A[i] = x
+
+eps = 5
+
+filename = 'image2'
+A = cv2.imread('images/%s.png' % filename)[:, :, 0].astype(float)
 initial_nonzeros = np.where(A!=0)[0].shape[0]
 
 compress_image(A)
@@ -25,6 +38,12 @@ compress_image(A)
 A[np.where(np.abs(A) < eps)] = 0
 
 approx_nonzeros = np.where(A!=0)[0].shape[0]
-print(initial_nonzeros)
-print(approx_nonzeros)
+
 print(float(initial_nonzeros) / approx_nonzeros)
+
+decompress_image(A)
+
+cv2.imwrite('%s_compressed.png' % filename, A.astype(np.uint8))
+
+# cv2.imshow('img', A.astype(np.uint8))
+# cv2.waitKey(0)
