d47b991addc11b6c01c81162a36e391f1221f355
Dima Petruk <bavaria95@gmail.com>
2016-11-08 19:39:09 +0100

    compression and zeroing all under epsilon

--- transformations/compression.py
+++ transformations/compression.py
@@ -2,24 +2,29 @@ import numpy as np
 import pywt
 import cv2
 
+eps = 5
+
+def compress_image(A, levels=3):
+    for i in range(A.shape[0]):
+        x = A[i].astype(float)
+        n = x.shape[0]
+        for _ in range(levels):
+            cA, cD = pywt.dwt(x[ :n], 'haar')
+            x[0:n/2] = cA
+            x[n/2:n] = cD
+            n /= 2
+
+        A[i] = x
+
+
 A = cv2.imread('images/image3.png')[:, :, 0].astype(float)
-print(A.shape)
+initial_nonzeros = np.where(A!=0)[0].shape[0]
 
-for i in range(A.shape[0]):
-    x = A[i].astype(float)
-    n = x.shape[0]
-    for _ in range(3):
-        cA, cD = pywt.dwt(x[ :n], 'haar')
-        x[0:n/2] = cA
-        x[n/2:n] = cD
-        n /= 2
-        # print(x)
-        print(np.where(abs(x) < 5)[0].shape)
-    print
+compress_image(A)
 
-    A[i] = x
+A[np.where(np.abs(A) < eps)] = 0
 
-print(A)
-
-
-
+approx_nonzeros = np.where(A!=0)[0].shape[0]
+print(initial_nonzeros)
+print(approx_nonzeros)
+print(float(initial_nonzeros) / approx_nonzeros)
