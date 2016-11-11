d47a396184b0ed62ef1b3d9ca1b407a448af7d9f
Dima Petruk <bavaria95@gmail.com>
2016-11-11 21:34:39 +0100

    applying DWT to columns after

--- transformations/compression.py
+++ transformations/compression.py
@@ -18,6 +18,17 @@ def compress_image(A, wavelet='haar'):
 
         A[i] = x
 
+    for i in range(A.shape[1]):
+        x = A[:, i]
+        n = x.shape[0]
+        for _ in range(int(np.log2(n))):
+            cA, cD = pywt.dwt(x[ :n], wavelet)
+            x[0:n/2] = cA
+            x[n/2:n] = cD
+            n /= 2
+
+        A[:, i] = x
+
 def decompress_image(A, wavelet='haar'):
     for i in range(A.shape[0]):
         x = A[i].astype(float)
@@ -29,6 +40,17 @@ def decompress_image(A, wavelet='haar'):
             n *= 2
 
         A[i] = x
+
+    for i in range(A.shape[1]):
+        x = A[:, i].astype(float)
+        n = 1
+        for _ in range(int(np.log2(x.shape))):
+            cA = x[0:n]
+            cD = x[n:2*n]
+            x[ :2*n] = pywt.idwt(cA, cD, wavelet)
+            n *= 2
+
+        A[:, i] = x
 
 eps = 5
 
