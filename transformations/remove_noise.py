7dbbebf576a2520b6139ee3fa965969e48928c4a
Dima Petruk <bavaria95@gmail.com>
2016-11-12 15:43:52 +0100

    decomposition

--- transformations/remove_noise.py
+++ transformations/remove_noise.py
@@ -3,15 +3,18 @@ import scipy
 import pywt
 import cv2
 
+def denoise(A, wavelet='haar'):
+    wavelet = pywt.Wavelet(wavelet)
+
+    coefs = pywt.wavedec2(A, wavelet)
+
+    threshold = 2*sqrt(2*log2(A.size))
+    coefs = map(lambda x: pywt.threshold(x, threshold), coefs)
+    A = pywt.waverec2(coefs, wavelet)
+
+    return A
+
 filename = 'image4_gauss'
 A = cv2.imread('images/%s.png' % filename)[:, :, 0].astype(float)
-
-wavelet = pywt.Wavelet('haar')
- 
-coefs = pywt.wavedec2(A, wavelet)
-
-threshold = 2*sqrt(2*log2(A.size))
-coefs = map(lambda x: pywt.threshold(x, threshold), coefs)
-A = pywt.waverec2(coefs, wavelet)
-
+A = denoise(A, 'haar')
 cv2.imwrite('%s_denoised.png' % filename, A)
