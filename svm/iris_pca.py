28bfd47408b46d7f8b3bf3bd2f2c18a8346bf710
Dima Petruk <bavaria95@gmail.com>
2016-11-20 00:51:04 +0100

    visualizing separation plane for 2D Iris

--- svm/iris_pca.py
+++ svm/iris_pca.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from sklearn import decomposition
 from sklearn import datasets
+from sklearn import svm
 
 iris = datasets.load_iris()
 X = iris.data
@@ -11,6 +12,25 @@ y = iris.target
 pca = decomposition.PCA(n_components=2)
 pca.fit(X)
 X = pca.transform(X)
+
+
+# fit the model
+clf = svm.SVC(kernel='linear')
+clf.fit(X, y)
+
+# get the separating hyperplanes
+w = clf.coef_[0]
+a = -w[0] / w[1]
+xx = np.linspace(-5, 5)
+yy = a * xx - (clf.intercept_[0]) / w[1]
+plt.plot(xx, yy, 'r-')
+
+w = clf.coef_[2]
+a = -w[0] / w[1]
+xx = np.linspace(-5, 5)
+yy = a * xx - (clf.intercept_[2]) / w[1]
+plt.plot(xx, yy, 'g-')
+
 
 y_0 = np.where(y==0)
 plt.plot(X[y_0, 0], X[y_0, 1], 'ro')
